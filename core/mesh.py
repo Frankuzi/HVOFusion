7c57f93b08f800edd9601d595dcdb8160335e786
Liu Shaofan <liushaofan@zju.edu.cn>
2024-09-21 02:25:08 +0800

    Update mesh.py

--- core/mesh.py
+++ core/mesh.py
@@ -11,7 +11,6 @@ class Mesh:
 
     def __init__(self, vertices, indices, colors, device='cpu'):
         self.device = device
-        # 将mc后的顶点和面保存为torch类型
         self.vertices = vertices.to(device, dtype=torch.float32) if torch.is_tensor(vertices) else torch.tensor(vertices, dtype=torch.float32, device=device)
         self.indices = indices.to(device, dtype=torch.int64) if torch.is_tensor(indices) else torch.tensor(indices, dtype=torch.int64, device=device) if indices is not None else None
         self.colors = colors.to(device, dtype=torch.float32) if torch.is_tensor(colors) else torch.tensor(colors, dtype=torch.float32, device=device) if colors is not None else None
@@ -60,8 +59,8 @@ class Mesh:
 
         assert len(vertices) == len(self.vertices)
 
-        mesh_new = Mesh(vertices, self.indices, self.colors, self.device)        # 只更新顶点 面不发生变化
-        mesh_new._edges = self._edges           # 仅改变顶点位置的时候 _edges _connected_faces _laplacian不会改变 因为indices拓扑结构不变
+        mesh_new = Mesh(vertices, self.indices, self.colors, self.device)        
+        mesh_new._edges = self._edges           
         mesh_new._edges_mask = self._edges_mask
         mesh_new._verts_mask = self._verts_mask
         mesh_new._connected_faces = self._connected_faces
@@ -71,24 +70,24 @@ class Mesh:
         return mesh_new
 
     @property
-    def edges(self):        # 计算三角面的各个边 并去除重复边
+    def edges(self):        
         if self._edges is None:
             from core.geometry import find_edges
-            self._edges, self._edges_mask, self._verts_mask = find_edges(self.indices, self.vertices)      # [2558, 3] 输入面的三个顶点对应verts中的index
+            self._edges, self._edges_mask, self._verts_mask = find_edges(self.indices, self.vertices)      
         return self._edges
 
     @property
     def connected_faces(self):
         if self._connected_faces is None:
             from core.geometry import find_connected_faces
-            self._connected_faces = find_connected_faces(self.vertices, self.indices, self.face_normals)  # 找到每个边连接的两个面 [3837, 2] 第一维表示边序号
+            self._connected_faces = find_connected_faces(self.vertices, self.indices, self.face_normals)  
         return self._connected_faces
 
     @property
     def laplacian(self):
         if self._laplacian is None:
             from core.geometry import compute_laplacian_uniform
-            self._laplacian = compute_laplacian_uniform(self)   # [1273, 1273]
+            self._laplacian = compute_laplacian_uniform(self)  
         return self._laplacian
     
     @property
@@ -97,8 +96,7 @@ class Mesh:
             import planar
             self._planar, self._planar_normals = planar.findPlanar(self.indices, self.vertex_normals, self.face_normals, 0.9999)
         return self._planar, self._planar_normals
-            
-    # 计算mesh的连接性 
+             
     def compute_connectivity(self):
         self._edges = self.edges
         self._laplacian = self.laplacian
@@ -107,14 +105,11 @@ class Mesh:
 
     def compute_normals(self):
         # Compute the face normals
-        # vertices[1273, 3] 顶点坐标
-        # indices[2558, 3] 每个面对应的顶点index
-        a = self.vertices[self.indices][:, 0, :]    # 取出三角面的三个顶点 [2558, 3]
+        a = self.vertices[self.indices][:, 0, :]    
         b = self.vertices[self.indices][:, 1, :]
         c = self.vertices[self.indices][:, 2, :]
-        self.face_normals = torch.nn.functional.normalize(torch.cross(b - a, c - a), p=2, dim=-1)   # 根据三角形计算三角形的法线 [2558, 3]
+        self.face_normals = torch.nn.functional.normalize(torch.cross(b - a, c - a), p=2, dim=-1)   
         '''
-        计算三角面的法线可以使用torch中的cross函数。假设三角面的三个点分别为$A(x_1,y_1,z_1)$，$B(x_2,y_2,z_2)$和$C(x_3,y_3,z_3)$，则可以使用以下代码计算法线：
 
         import torch
 
@@ -130,10 +125,10 @@ class Mesh:
 
         # Compute the vertex normals
         vertex_normals = torch.zeros_like(self.vertices)
-        vertex_normals = vertex_normals.index_add(0, self.indices[:, 0], self.face_normals) # [1273, 3] 顶点法线
+        vertex_normals = vertex_normals.index_add(0, self.indices[:, 0], self.face_normals) 
         vertex_normals = vertex_normals.index_add(0, self.indices[:, 1], self.face_normals)
-        vertex_normals = vertex_normals.index_add(0, self.indices[:, 2], self.face_normals) # 每个顶点的法线为 该顶点所涉及的面的法线归一化后的结果
-        self.vertex_normals = torch.nn.functional.normalize(vertex_normals, p=2, dim=-1)    # 对每个顶点法线进行归一化
+        vertex_normals = vertex_normals.index_add(0, self.indices[:, 2], self.face_normals) 
+        self.vertex_normals = torch.nn.functional.normalize(vertex_normals, p=2, dim=-1)    
 
 def face_areas_normals(faces, vs):
     face_normals = torch.cross(vs[:, faces[:, 2], :] - vs[:, faces[:, 1], :],
